2433d0194e427424c299dd8648c361b276c78b25
MohammadAminJeddi <167413082+MohammadAminJeddi@users.noreply.github.com>
2024-04-20 08:33:35 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+
+
+public struct person
+{
+    public String Firstname;
+    public String Lastname;
+    public int hight;
+    public String job;
+}
+
+class WorkWithFiles
+{
+    public static void Main(string[] args)
+    {
+        person p1 = new person();
+
+        Console.WriteLine("Enter first name:\n");
+        p1.Firstname = Console.ReadLine();
+        Console.WriteLine("Enter last name:\n");
+        p1.Lastname = Console.ReadLine();
+        Console.WriteLine("Enter hight:\n");
+        p1.hight = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter job:\n");
+        p1.job = Console.ReadLine();
+
+
+        String personDetails = JsonConvert.SerializeObject(p1);
+        String path = "person.txt";
+        if (File.Exists(path))
+        {
+            var f = File.Create(path);
+            f.Close();
+        }
+        File.WriteAllText(path, personDetails);
+
+
+        /////////////////////////////////////////////////////////////////////
+        ///
+
+        String read = File.ReadAllText(path);
+        person p2 = new person();
+        p2 = JsonConvert.DeserializeObject<person>(read);
+        Console.WriteLine(p2.Firstname + " , " + p2.job);
+        
+        Thread.Sleep(10000);
+
+        
+        
+
+    }
+}
